2a3007aaf8f2e508cc6aa2b9863e83756c9713e6
DanielOtto <danieels.otto@gmail.com>
2024-02-28 15:56:13 +0200

    Finished logic of saving project to DB and displaying to on UI

--- src/main/java/com/app/model/Project.java
+++ src/main/java/com/app/model/Project.java
@@ -5,6 +5,13 @@ public class Project {
     private String projectName;
     private String color;
 
+    public Project(long id, String projectName, String color) {
+        this.id = id;
+        this.projectName = projectName;
+        this.color = color;
+    }
+    public Project(){};
+
     public long getId() {
         return id;
     }
